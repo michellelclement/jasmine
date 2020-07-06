b1263f1e07b003814f485aa24d2072d204cf5f97
Michelle Clement <michellelclement@gmail.com>
2020-07-06 08:43:39 +0000

    Final code for Jasmine module

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,12 +1,11 @@
 Calculator = function() {
     this.value = 0;
-}
+};
 
-
-Calculator.prtotype.add = fucntion(number){
-    if(typeof(number) == "number") {
+Calculator.prototype.add = function(number) {
+    if (typeof(number) == "number") {
         this.value += number;
     } else {
-        alert("error!");
+        alert("Error!");
     }
-}
+};
